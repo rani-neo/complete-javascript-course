28f1ba81b4299c5f4d67c392fadfe01e7106a577
rani-neo <ishika.rashmi@gmail.com>
2024-05-02 10:14:25 +1000

    commited

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,9 +1,21 @@
- 
-    let js = 'amazing';
-    console.log(40 + 8 + 23 - 10);
-    
- console.log("Rashmi");
+  let js = 'amazing';
+  console.log(40 + 8 + 23 - 10);
+  
+console.log("Rashmi");
 console.log(40);
- 
-let firstName = "Rashmi";
+let firstName = "Ishaan";
+
 console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let rashmi_ishaan = "RI"
+let $function = 27;
+let person = 'jones'
+let PI = 3.1415;
+
+let myFirstJob = "Admin";
+let myCurrentJob = "DevOps";
+console.log(myFirstJob);
+
+
